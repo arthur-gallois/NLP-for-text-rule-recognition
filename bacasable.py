74cd4f963dd7a4b5923677838b042e87af8eb6c7
Dumont Eliott <eliott.dumont@student-cs.fr>
2022-03-21 16:02:31 +0100

    commit de tout

--- bacasable.py
+++ bacasable.py
@@ -1,14 +1,17 @@
 import docx
-L=[]
-doc = docx.Document("test_doc.docx")
 
-bolds=[]
-italics=[]
+doc = docx.Document("test_doc.docx")
+k = len(doc.paragraphs)
+bolds=[[] for i in range(k)]
+italics=[[] for i in range(k)]
+p=0
 for para in doc.paragraphs:
     for run in para.runs:
         if run.italic :
-            italics.append(run.text)
+            italics[p].append(run.text)
         if run.bold :
-            bolds.append(run.text)
-            
-print(bolds, italics)
+            bolds[p].append(run.text)
+    p += 1
+print(bolds)
+
+
