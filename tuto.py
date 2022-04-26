728f0bd66bba79f6371377e02473e77adb8c040a
Dumont Eliott <eliott.dumont@student-cs.fr>
2022-04-26 14:56:23 +0200

    ajout de tuto.py

--- tuto.py
+++ tuto.py
@@ -1,1 +1,70 @@
 import tensorflow_hub as hub
+import pandas as pd
+import tokenization
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1"
+
+bert_layer = hub.KerasLayer(module_url, trainable=True)
+
+train = pd.read_csv("C:/Users/eliot/BERT_Easy_Implementation/data/train.csv")
+test = pd.read_csv("C:/Users/eliot/BERT_Easy_Implementation/data/test.csv")
+train.head(3)
+
+
+
+def bert_encode(texts, tokenizer, max_len=512):
+    all_tokens = []
+    all_masks = []
+    all_segments = []
+    
+    for text in texts:
+        text = tokenizer.tokenize(text)
+            
+        text = text[:max_len-2]
+        input_sequence = ["[CLS]"] + text + ["[SEP]"]
+        pad_len = max_len - len(input_sequence)
+        
+        tokens = tokenizer.convert_tokens_to_ids(input_sequence)
+        tokens += [0] * pad_len
+        pad_masks = [1] * len(input_sequence) + [0] * pad_len
+        segment_ids = [0] * max_len
+        
+        all_tokens.append(tokens)
+        all_masks.append(pad_masks)
+        all_segments.append(segment_ids)
+    
+    return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
+
+
+vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
+tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
+
+train_input = bert_encode(train.text.values, tokenizer, max_len=100)
+test_input = bert_encode(test.text.values, tokenizer, max_len=100)
+train_labels = train.label.values
+test_labels = test.label.values
+
+
+
+def build_model(bert_layer, max_len=512):
+    input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
+    input_mask = Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
+    segment_ids = Input(shape=(max_len,), dtype=tf.int32, name="segment_ids")
+
+    _, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
+    clf_output = sequence_output[:, 0, :]
+    out = Dense(1, activation='sigmoid')(clf_output)
+    
+    model = Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
+    model.compile(Adam(lr=2e-6), loss='binary_crossentropy', metrics=['accuracy'])
+    
+    return model
+
+model = build_model(bert_layer, max_len=100)
