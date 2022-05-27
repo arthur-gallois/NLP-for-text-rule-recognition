bce19267e669fc3b408e6ab306f23be5898e0e29
NathanChalumeau <nathan.chalumeau@gmail.com>
2022-05-27 12:50:07 +0200

    maj methode logique

--- naive_stanza.py
+++ naive_stanza.py
@@ -1,17 +1,14 @@
 import stanza
 # stanza.download('en')
 nlp = stanza.Pipeline(
-        'en', processors='tokenize,mwt,pos,lemma,depparse,ner,sentiment,constituency')
-liste_mots = ['if', 'unless', 'otherwise', 'case']
-
+    'en', processors='tokenize,mwt,pos,lemma,depparse,ner,sentiment,constituency')
+liste_mots = ['if', 'unless', 'otherwise', 'case','when']
 
 def take_id(mot):
     return mot[1]
 
-
 def take_text(mot):
     return mot[0]
-
 
 def fils(phrase):
     n = len(phrase)
@@ -19,7 +16,6 @@ def fils(phrase):
     for mot in phrase:
         if mot['head'] != 0:
             aux[mot['head']-1].append(mot['id'])
-
 
 def exception(phrase, mot):
     if mot['id'] != 1:
@@ -31,11 +27,9 @@ def exception(phrase, mot):
                 return True
     return False
 
-
 def cause_consequences(text):
-    compt = 0
     doc = nlp(text)
-    for p in range(len(doc.sentences)):
+    for p in range(len(doc.sentences)): #on parcours les phrases du texte
         phrase = doc.sentences[p].to_dict()
         if phrase[-1]['text'] == '?':  # on verifie que le phrase n'est pas une question
             print('C\'est un question')
@@ -49,89 +43,32 @@ def cause_consequences(text):
                 h1 = mot['head']
                 if exception(phrase, mot):
                     pass
-                else:
-                    # action est le mot principal de la phrase
-                    action = phrase[h1-1]
+                else:            
+                    action = phrase[h1-1]   #action est le mot principal de la phrase
                     h2 = action['head']
-                    if h2 != 0:
-                        consequence = phrase[h2-1]['text']
+                    if h2 != 0:             #si on a une conséquence
+                        consequence = phrase[h2-1]['text']  #consequence est un mot important de la onséquence
                         consequences.append((consequence, h2))
                         consequences_id.append(h2)
                     causes.append((action['text'], h1))
                     causes_id.append(h1)
-            if len(causes) != 0:
-                compt += 1
-                for k in range(2):
+            if len(causes) != 0:    #si on a repéré une cause
+                boole=True
+                while boole:#tant que l'on rajoute des élements on continue
+                    boole=False
                     for mot in phrase:
                         h = mot['head']
                         id = mot['id']
-                        # si le mot appartient à la cause
-                        if h in (causes_id) and (id not in causes_id) and (mot['upos'] != "PUNCT"):
+                        if h in (causes_id) and (id not in causes_id) and (mot['upos'] != "PUNCT"):# si le mot appartient à la cause
                             causes.append((mot['text'], mot['id']))
                             causes_id.append(mot['id'])
-                        # si le mot appartient à la conséquence
-                        if (h in consequences_id) and (id not in consequences_id) and (h2 != 0) and (mot['id'] != h1) and (mot['upos'] != "PUNCT"):
+                            boole=True
+                        if (h in consequences_id) and (id not in consequences_id) and (h2 != 0) and (mot['id'] != h1) and (mot['upos'] != "PUNCT"):# si le mot appartient à la conséquence
                             consequences.append((mot['text'], mot['id']))
                             consequences_id.append(mot['id'])
-                causes.sort(key=take_id)
-                consequences.sort(key=take_id)
-                causes = list(map(take_text, causes))
+                            bool=True
+                causes.sort(key=take_id)    #on trie la cause pour retrouver l'ordre présent dans la phrase
+                consequences.sort(key=take_id)  #de même
+                causes = list(map(take_text, causes)) 
                 consequences = list(map(take_text, consequences))
-                # print('Cause:')
-                # print(causes)
-                # print('Conséquence:')
-                # print(consequences)
-                # print(compt)
                 return causes, consequences
-
-
-p1 = 'If your players will be wearing cleats, make sure they are soccer cleats.'
-p2 = 'If food is foil wrapped, remove foil and place it in a suitable container.'
-p3 = 'Unless the cookies aren\'t in the oven, start it'
-p4 = 'You can determine if it\'s clogged by looking at your pool water'
-p5 = 'Even if you are careful, pool chemicals, the sun, and time will eventually ruin the liner'
-p6 = 'When opening your pool, take note that if there are no algae to kill, the algaecide will create some foam.'
-p7 = 'If you are receiving an error message or an unexpected result, it is easy to include a log file that demonstrates the problem'
-p8 = 'If you had 100 variables like that, the savings would be 300 bytes per observation, and if you had 3000000 observations, the total savings would be nearly 900 megabytes.'
-text2 = '''Power on or restart the computer and listen carefully to the beeps.
-Write down the number of beeps and whether they are long, short, or of equal length. Also, make a note if the beeps repeat.
-Install a tool to determine the BIOS maker and then consult the appropriate online troubleshooting guide.
-This article explains how to figure out why your PC is beeping by noting the beep pattern, determining your computer's BIOS maker, and consulting the matching online guide.
-
-How to Troubleshoot Beep Codes
-If you're hearing beep codes after you turn your computer on and then it doesn't start—it means the motherboard encountered some kind of problem before it was able to send any error information to the monitor.
-
-Follow these steps below to determine what computer problem the beep code is representing. Once you know what's wrong, you can work to fix the problem.
-
-Power on the computer or restart it if it's already on.
-
-Listen very carefully to the beep codes that sound when the computer begins to boot.
-
-Restart your computer if you need to hear the beeping again. You're probably not going to make whatever problem you have worse by restarting a few times.
-
-Write down, in whatever way makes sense to you, how the beeps sound.
-
-Pay close attention to the number of beeps, if the beeps are long or short (or all the same length), and if the beeping repeats or not. There is a big difference between a "beep-beep-beep" and a "beep-beep."
-
-Yes, this might all seem a little crazy, but this is important information that will help determine what issue the beep codes are representing. If you get this wrong, you'll be trying to solve a problem your computer doesn't have and ignoring the real one.
-
-Next, you'll need to figure out what company manufactured the BIOS chip that's on your computer motherboard. Unfortunately, the computer industry never agreed on a uniform way to communicate with beeps, so it's important to get this right.
-
-The easiest way to figure this is out is by installing one of a free system information tool, which should tell you if your BIOS is made by AMI, Award, Phoenix, or another company. If that doesn't work, you could open your computer and take a peek at the actual BIOS chip on your computer motherboard, which should have the company name printed on or next to it.
-
-Your computer maker isn't the same as the BIOS maker and your motherboard maker isn't necessarily the same as the BIOS maker, so don't assume you already know the right answer to this question.
-
-Now that you know the BIOS manufacturer, choose the troubleshooting guide below based on that information:
-
-AMI Beep Code Troubleshooting (AMIBIOS)
-Award Beep Code Troubleshooting (AwardBIOS)
-Phoenix Beep Code Troubleshooting (PhoenixBIOS)
-Using the beep code information specific to those BIOS makers in those articles, you'll be able to figure out exactly what's wrong that's causing the beeping, be it a RAM issue, a video card problem, or some other hardware problem.
-
-More Help With Beep Codes
-Some computers, even though they may have BIOS firmware made by a particular company, like AMI or Award, further customize their beep-to-problem language, making this process a little frustrating. If you think this might be the case, or just worried it could be, almost every computer maker publishes their beep code list in their user guides, which you can probably find online.
-
-If you need help digging up your computer's manual, go online to find tech support information.
-'''
-
-cause_consequences(text2)
--- test_naive_stanza.py
+++ test_naive_stanza.py
@@ -3,35 +3,31 @@ from sympy import N
 from dataset_utility import *
 from naive_stanza import *
 
-text = getJson('dataset')
-# print(len(text.rules[0].condition))
-ratio_bon_cause = 0
-ratio_nul_cause = 0
-ratio_bon_conseq = 0
-ratio_nul_conseq = 0
+text = getJson('data2')
+ratio = [0, 0, 0, 0]    #ratio_bon_cause/ratio_mauvais_cause/ratio_bon_consequence/ratio_mauvais_consequence
 n = len(text.rules)
-for k in range(n):
+for k in range(n):      #on boucle sur les phrases
     cause, consequence = cause_consequences(text.rules[k].text)
     bon_mots = 0
     mauvais_mots = 0
-    for mot in cause:
+    for mot in cause:   #on compare les causes
         if mot in text.rules[k].condition:
             bon_mots += 1
         else:
             mauvais_mots += 1
-    ratio_bon_cause += bon_mots/len(text.rules[k].condition)
-    ratio_nul_cause += mauvais_mots/len(cause)
+    ratio[0] += bon_mots/len(text.rules[k].condition)
+    ratio[1] += mauvais_mots/len(cause)
     bon_mots = 0
     mauvais_mots = 0
-    for mot in consequence:
+    for mot in consequence: #on compare les conséquences
         if mot in text.rules[k].consequence:
             bon_mots += 1
         else:
             mauvais_mots += 1
-    ratio_bon_conseq += bon_mots/len(text.rules[k].condition)
-    ratio_nul_conseq += mauvais_mots/len(cause)
-ratio_bon_cause *= 100/n
-ratio_nul_cause *= 100/n
-ratio_bon_conseq *= 100/n
-ratio_nul_conseq *= 100/n
-print(ratio_bon_cause, ratio_nul_cause, ratio_bon_conseq, ratio_nul_conseq)
+    ratio[2] += bon_mots/len(text.rules[k].condition)
+    ratio[3] += mauvais_mots/len(cause)
+ratio[0] *= 100/n   #on exprime les ratios en %
+ratio[1] *= 100/n
+ratio[2] *= 100/n
+ratio[3] *= 100/n
+print(ratio)
